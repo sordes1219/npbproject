0140c82bfe2629a76ecba712c651827017998b76
sordes <sordes@example.com>
2019-10-22 01:35:38 +0900

    improve forms

--- blog/forms.py
+++ blog/forms.py
@@ -5,4 +5,7 @@ class PostForm(forms.ModelForm):
 
     class Meta:
         model = Pitchingdata
-        fields = ('batter',)
+        fields = ('balltype','batter',)
+
+class NameForm(forms.Form):
+    your_name = forms.CharField(label='Your name', max_length=100)
--- blog/views.py
+++ blog/views.py
@@ -3,12 +3,19 @@ from .models import Pitchingdata
 from .forms import PostForm
 from django_pandas.io import read_frame
 
+
 def top(request):
     if request.method == "POST":
-        batter = request.POST.get('batter',None)
-        df = read_frame(Pitchingdata.objects.all(),fieldnames=['kyusyumark','pitcher','batter'])
-        df = df.loc[df['batter']==batter,:]
-        return render(request,'blog/detail.html',{'df':df})
+        form = PostForm(request.POST)
+        if form.is_valid():
+            balltype = form.cleaned_data['balltype']
+            batter = form.cleaned_data['batter']
+            df = read_frame(Pitchingdata.objects.all())
+            df = df.loc[(df['batter']==batter) & (df['balltype']==balltype)]
+            return render(request,'blog/detail.html',{'df':df})
     else:
         form = PostForm()
-        return render(request,'blog/top.html',{'form':form})
+        batter_list = set(Pitchingdata.objects.values_list('batter',flat=True))
+        balltype_list = set(Pitchingdata.objects.values_list('balltype',flat=True))
+
+    return render(request,'blog/top.html',{'form':form,'batter':batter_list,'balltype':balltype_list})
